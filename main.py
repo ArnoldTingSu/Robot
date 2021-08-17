81564898f5cc7b65dec5650bb7ed3479d94de783
Arnold Su <arnoldtingsu@gmail.com>
2021-08-17 02:16:04 +0000

    keywords and replies

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
 import os
 import discord
+import requests
+import json
+import random
+
+
 my_secret = os.environ['COIN']
 
 
 client = discord.Client()
 
+queryWords= [
+  "question",
+  "explore",
+  "lookup",
+  "find",
+  "what",
+  "need",
+  "suggestion",
+  "suggestions"
+]
+
+queryDict = {
+  "question": "questions",
+  "help": "help me"
+}
+
+queryStarterConvo = [
+  "I know a solution",
+  "I may have a suggestion...",
+  "I can help you with that!",
+  "I'm a helper, I can look that up for you!"
+]
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " - " + json_data[0]['a']
+  return(quote)
+
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 
 @client.event
@@ -15,10 +51,20 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("$hello"):
+    msg = message.content
+
+    if msg.startswith("$hello"):
         await message.channel.send("hello!")
     
-    if message.content.startswith("bye"):
+    if msg.startswith("$bye"):
       await message.channel.send("goodbye")
+
+    if msg.startswith("$quote"):
+      quote= get_quote()
+      await message.channel.send(quote)
+
+    if any(word in msg for word in queryWords):
+      await message.channel.send(random.choice(queryStarterConvo))
+
 
 client.run(os.getenv('COIN'))
